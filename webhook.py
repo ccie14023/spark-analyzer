d53e9ffecb9ba9d59c239e24dcf3b331ebcf15ba
Jeff McLaughlin <jeff@jeffmc.net>
2018-02-10 15:23:01 -0800

    Initial commit

---
+++ webhook.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def foo():
+	print request.data
+#   data = json.loads(request.data)
+#   print "New commit by: {}".format(data['commits'][0]['author']['name'])
+   return "OK"
+
+if __name__ == '__main__':
+   app.run()
