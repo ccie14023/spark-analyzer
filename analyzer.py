c25534abdbd5344097f1b03331de781632111468
Jeff McLaughlin <jeff@jeffmc.net>
2018-02-11 09:01:10 -0800

    added main to analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,25 @@
 from wordcloud import WordCloud
+from textblob import TextBlob
+import os
+
+def get_sentiment(text):
+	f = open("messages","r")
+	text = f.read()
+	f.close()
+	blob = TextBlob(text)
+	return blob.sentiment.polarity
+
+def make_cloud(text):
+	wordcloud = WordCloud().generate(text)
+	wordcloud.to_file("wc.png")
+
+if __name__ == "__main__":
+	
+	f = open("messages","r")
+	text = f.read()
+	f.close()
+
+	os.remove("wc.png")
+
+	print get_sentiment(text)
+	make_cloud(text)
