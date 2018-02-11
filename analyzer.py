92b45de9a48fa7cc3d29fb566a3a33713f3c6669
Jeff McLaughlin <jeff@jeffmc.net>
2018-02-11 09:10:05 -0800

    uc

--- analyzer.py
+++ analyzer.py
@@ -16,7 +16,7 @@ def make_cloud(text):
 if __name__ == "__main__":
 	
 	f = open("messages","r")
-	text = f.read().encode("ascii","ignore")
+	text = f.read().encode("utf-8","ignore")
 	f.close()
 
 	os.remove("wc.png")
